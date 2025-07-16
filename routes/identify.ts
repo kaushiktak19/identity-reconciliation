280a40117e54ca4b39feaa61b29830c74b434131
kaushiktak19 <kaushiktak19@gmail.com>
2025-07-16 23:50:12 +0530

    inital setup & server file

---
+++ routes/identify.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import pool from '../db/db'
+
+const identifyRoute = express.Router()
+
+identifyRoute.post('/', async (req, res) => {
+  
+})
+
+export default identifyRoute
